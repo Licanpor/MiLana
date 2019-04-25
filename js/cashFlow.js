de6d3de00db423708850bc203ac44ced5297d20d
Licanpor <liliana.canor@gmail.com>
2019-04-25 01:32:22 -0500

    date

--- js/cashFlow.js
+++ js/cashFlow.js
@@ -1,10 +1,10 @@
 var objToday = new Date(),
 	weekday = ['Lunes', 'Martes', 'Miércoles', 'Juves', 'Viernes', 'Sábado', 'Domingo'];
 	dayOfWeek = weekday[objToday.getDay()],
-	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "ero"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	//domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "ero";
+	dayOfMonth = today + objToday.getDate() < 10 ? '0' + objToday.getDate() : objToday.getDate();
 	months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-	curMonth = months[objToday.getMonth()],
-var today = dayOfWeek + " " + dayOfMonth + " de " + curMonth;
+	curMonth = months[objToday.getMonth()];
+var today = dayOfWeek + ", " + dayOfMonth + " de " + curMonth;
 
-document.getElementsByTagName('h1')[0].textContent = today;
+document.getElementsByClassName('current_date')[0].textContent = today;
