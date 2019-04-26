faee9cd09ce9551897661f52ac03a4572a3240c8
GirlLollipop <>
2019-04-26 04:17:03 -0500

    ultimos cambios de estilo

--- js/cashFlow.js
+++ js/cashFlow.js
@@ -1,5 +1,5 @@
-var objToday = new Date();
-	weekday = ['Lunes', 'Martes', 'Miércoles', 'Juves', 'Viernes', 'Sábado', 'Domingo'];
+	var objToday = new Date();
+	weekday = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 	dayOfWeek = weekday[objToday.getDay()];
 	dayOfMonth = today + objToday.getDate() < 10 ? '0' + objToday.getDate() : objToday.getDate();
 	months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
@@ -7,3 +7,11 @@ var objToday = new Date();
 var today = dayOfWeek + ", " + dayOfMonth + " de " + curMonth;
 
 document.getElementsByClassName('current_date')[0].textContent = today;
+
+
+/*
+function balanceActualitation() {
+	var cash = document.getElementById("balanceCash").innerHTML;
+	var spen = document.getElementsById("spending").value;
+}
+*/
