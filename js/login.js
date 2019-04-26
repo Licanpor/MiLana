0424f6fda58c7f1db14d7e0e2d882534d9cb71e8
Licanpor <liliana.canor@gmail.com>
2019-04-26 05:26:58 -0500

    done login

--- js/login.js
+++ js/login.js
@@ -1,23 +1,20 @@
 var provider = new firebase.auth.FacebookAuthProvider();
+var userPic = document.querySelectorAll('userInfo > div')
+var userName = document.querySelectorAll('userInfo > p')
+
 function loginFb(){
-	console.log('click')
-	
 	provider.addScope('public_profile');
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 		var token = result.credential.accessToken;
 		var user = result.user;
 		alert(user)
 		window.location = '/views/dashboard.html';
-		M.toast(`¡¡Hola, ${result.user.displayName} !!`, 4000)
+		window.setTimeout(M.toast(`¡¡Hola, ${result.user.displayName} !!`, 3000), 1500)
+		userPic.style.backgroundImage = result.user.photoURL
+		userName.style.innerHTML = result.user.displayName
 	}).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// The email of the user's account used.
-		var email = error.email;
-		// The firebase.auth.AuthCredential type that was used.
-		var credential = error.credential;
-		// ...
+		console.log(error)
+		M.toast(`Error al autenticarse con Facebook ${error} !!`)
 	});
 }
 document.getElementById('authFB').addEventListener('click', loginFb, false)
