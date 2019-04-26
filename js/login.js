a9c1297da0bbff42ae8d544536caddf9124a863c
Licanpor <liliana.canor@gmail.com>
2019-04-26 05:58:12 -0500

    fix dom issue

--- js/login.js
+++ js/login.js
@@ -1,16 +1,15 @@
 var provider = new firebase.auth.FacebookAuthProvider();
-var userPic = document.querySelectorAll('userInfo > div')
-var userName = document.querySelectorAll('userInfo > p')
-
 function loginFb(){
 	provider.addScope('public_profile');
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 		var token = result.credential.accessToken;
 		var user = result.user;
+		document.addEventListener('DOMContentLoaded', function() {
+			var elems = document.querySelectorAll('#loginWelcomeModal');
+			var instances = M.Modal.init(elems, options);
+		});
+		document.querySelectorAll('#loginWelcomeModal h4').innerHTML = `¡¡Hola, ${result.user.displayName} !!`
 		window.location = './views/dashboard.html';
-		window.setTimeout(M.toast(`¡¡Hola, ${result.user.displayName} !!`, 3000), 1500)
-		userPic.style.backgroundImage = result.user.photoURL
-		userName.style.innerHTML = result.user.displayName
 	}).catch(function(error) {
 		console.log(error)
 		M.toast(`Error al autenticarse con Facebook ${error} !!`)
