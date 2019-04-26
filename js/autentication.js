9899955fc95f803b2c6b46c087b1efebbf41135d
Licanpor <liliana.canor@gmail.com>
2019-04-26 03:03:06 -0500

    login

--- js/autentication.js
+++ js/autentication.js
@@ -1,18 +1,33 @@
-
-const objAuth = new Autentication()
-$("#authFB").click(() => objAuth.authCuentaFacebook());
-
-
-
-authCuentaFacebook() {
-  const provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(result => {
-    $('#avatar').attr('src',result.user.photoURL),
-    $('#modal').modal('close'),
-    Materialize.toast(`Bienvenido ${result.user.displayName}!!`,4000)
-  })
-  .catch(error => {
-    console.log(error)
-    Materialize.toast(`Error: ${error}!!`,4000)
-  })
+function loginFb(){
+  if(!firebase.auth().currentUser){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    //provider.addScope('public_profile')
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log(user)
+      window.location = '/views/dashboard.html';
+      M.toast(`¡¡Hola, ${result.user.displayName} !!`, 4000)
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  } else {
+    console.log('OUT!')
+    firebase.auth().signOut();
+  }
 }
+ document.getElementById('authFB').addEventListener('click', loginFb(), false)
+
+
+
+
+
+
